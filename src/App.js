4613086b0cf28b5bc78c1ad673eda70bb8b8330b
Aleksandra <a.haniok@gmail.com>
2023-11-07 19:47:08 +0000

    Build the Static App: Forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const initialFriends = [
   {
     id: 118836,
     name: 'Laura',
-    image: 'https://i.pravatar.cc/1786577',
+    image: 'https://i.pravatar.cc/48?u=171211',
     balance: -7,
   },
   {
@@ -24,8 +24,11 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
-      ;
+
+      <FormSplitBill />
     </div>
   );
 }
@@ -46,6 +49,7 @@ function Friend({ friend }) {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
       {friend.balance < 0 && (
         <p className="red">
           {friend.name} owes you £{Math.abs(friend.balance)}
@@ -58,7 +62,50 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend name</label>
+      <input type="text" />
+
+      <label>🖼️Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X </h2>
+
+      <label>💵 Bill value</label>
+      <input type="text" />
+
+      <label>🕴️ Your expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
